30fb6835951c9cb13a8245b372912d5026abcd6d
Sajith-S15 <connectsajithinreallife@gmail.com>
2024-03-07 12:35:17 +0530

    added terms

--- core/urls.py
+++ core/urls.py
@@ -9,6 +9,9 @@ app_name = 'core'
 urlpatterns = [
     path('', views.index, name='index'),
     path('contact/', views.contact, name='contact'),
+    path('about/', views.about, name='about'),
+    path('privacy/', views.privacy, name='privacy'),
+    path('terms/', views.terms, name='terms'),
     path('signup/', views.signup, name='signup'),
     path('login/', auth_views.LoginView.as_view(template_name='core/login.html', authentication_form=LoginForm), name='login'),
 ]
--- core/views.py
+++ core/views.py
@@ -16,6 +16,14 @@ def index(request):
 def contact(request):
     return render(request, 'core/contact.html')
 
+def about(request):
+    return render(request, 'core/about.html' )
+
+def privacy(request):
+    return render(request, 'core/privacy.html' )
+def terms(request):
+    return render(request, 'core/terms.html' )
+
 def signup(request):
     if request.method == 'POST':
         form = SignupForm(request.POST)
